48ac92093ca30c12f10f42d48f18471039445a32
deeja@users.noreply.github.com <deeja@users.noreply.github.com>
2019-06-02 10:56:46 +0100

    Add some info to the enums

--- 1_types/enums.js
+++ 1_types/enums.js
@@ -15,6 +15,8 @@ var Bear;
     Bear[Bear["Brown"] = 200] = "Brown";
     Bear[Bear["Grizzly"] = 1000] = "Grizzly";
 })(Bear || (Bear = {}));
+// The JS that is produced creates an object using index signatures
+// e.g. {1: "Sun", 100: "Black", 200: "Brown", 1000: "Grizzly", Sun: 1, Black: 100, Brown: 200, Grizzly: 1000}
 var bearChasingMe = Bear.Brown;
 // output number (as it's compiled to raw js)
 console.log("The Bear value is " + bearChasingMe);
--- 1_types/enums.ts
+++ 1_types/enums.ts
@@ -16,6 +16,9 @@ enum Bear {
     Grizzly = 1000    
 }
 
+// The JS that is produced creates an object using index signatures
+// e.g. {1: "Sun", 100: "Black", 200: "Brown", 1000: "Grizzly", Sun: 1, Black: 100, Brown: 200, Grizzly: 1000}
+
 let bearChasingMe = Bear.Brown;
 
 // output number (as it's compiled to raw js)
